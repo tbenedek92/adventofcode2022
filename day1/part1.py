6fbb18cbb66a9a341f594755b64a5c1996c71bc0
benedek <t.benedek92@gmail.com>
2022-12-02 15:57:03 +0100

    day1 part1

--- day1/part1.py
+++ day1/part1.py
@@ -1,11 +1,24 @@
-def main():
-    file_reader()
+def elf_max_calorie(elf_calorie_list):
+    elf_calorie = 0
+    max_elf_calorie = 0
+    for calorie in elf_calorie_list:
+        if calorie.isdigit():
+            elf_calorie += int(calorie)
+        else:
+            elf_calorie = 0
+
+        max_elf_calorie = max(elf_calorie, max_elf_calorie)
+
+    return max_elf_calorie
 
 
 def file_reader():
     with open('day1/input.txt') as f:
-        lines = f.read().splitlines()
-        print(lines)
+        data = f.read().splitlines()
+    return data
+
 
 if __name__ == '__main__':
-    main()
+    data = file_reader()
+    result = elf_max_calorie(data)
+    print(result)
